711fda92cc9eec24446391ed6ac9b63104a8a804
astolfovalentim <astolfom@gmail.com>
2022-04-23 11:53:42 -0300

    commit 1

---
+++ src/controllers/cat.controller.js
@@ -0,0 +1,39 @@
+const catsService = require("../services/cat.service");
+
+const findAllCatsController = (req, res) => {
+  const cats = catsService.findAllCatsService();
+  res.send(cats);
+};
+
+const findByIdCatController = (req, res) => {
+  const idParam = Number(req.params.id);
+  const chosenCat = catsService.findByIdCatService(idParam);
+  res.send(chosenCat);
+};
+
+const createCatsController = (req, res) => {
+  const cat = req.body;
+  const newCat = catsService.createCatsService(cat);
+  res.send(newCat);
+};
+
+const updateCatsController = (req, res) => {
+  const idParam = Number(req.params.id);
+  const catEdit = req.body;
+  const updatedCat = catsService.updateCatsService(idParam, catEdit);
+  res.send(updatedCat);
+};
+
+const deleteCatsController = (req, res) => {
+  const idParam = req.params.id;
+  catsService.deleteCatsService(idParam);
+  res.send({ message: "Thundercat deletado com sucesso!" });
+};
+
+module.exports = {
+  findAllCatsController,
+  findByIdCatController,
+  createCatsController,
+  updateCatsController,
+  deleteCatsController,
+};
---
+++ src/routes/cat.route.js
@@ -0,0 +1,10 @@
+const route = require("express").Router();
+const controllerCats = require("../controllers/cat.controller");
+
+route.get("/find-cats", controllerCats.findAllCatsController);
+route.get("/cat/:id", controllerCats.findByIdCatController);
+route.post("/create", controllerCats.createCatsController);
+route.put("/update/:id", controllerCats.updateCatsController);
+route.delete("/delete/:id", controllerCats.deleteCatsController);
+
+module.exports = route;
